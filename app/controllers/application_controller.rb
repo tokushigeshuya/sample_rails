9fb8473f4bb6cb00eba9e66e61abe060b291c844
Syuya Tokushige <tokushige-s@unitedgate.co.jp>
2023-10-19 12:44:31 +0900

    [deploy]デプロイ機能確認の為、変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
     #     render html: "¡Hola, mundo!"
     # end
     def goodbye
-        render html: "goodbye, world!"
+        render html: "本当にテストなんですよ"
     end
 end
